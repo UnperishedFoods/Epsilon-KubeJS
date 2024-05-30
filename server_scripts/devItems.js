9cada8c3127f4fcbae8ad0122e960a860818f725
UnperishedFoods <131722267+UnperishedFoods@users.noreply.github.com>
2024-05-30 14:04:29 +0000

    added recipe for the pickaxe of bob.

--- server_scripts/devItems.js
+++ server_scripts/devItems.js
@@ -1,3 +1,16 @@
 ServerEvents.recipes((event) => {
-  
+  event.shaped(
+    "epsilon:pickaxe_of_bob",
+    [
+      "ABA",
+      " C ",
+      " D ",
+    ],
+    {
+      A: "mekanism:block_uranium",
+      B: "minecraft:quartz_block",
+      C: "pneumaticcraft:logistics_core",
+      D: "minecraft:stick",
+    },
+  )
 })
